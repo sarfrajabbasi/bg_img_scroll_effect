8d9b5318227323f6f6734caab7e0d7cfaf57ce08
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-16 20:37:04 +0530

    scroll effect on image

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function createEl(el) {
+  try {
+    const element = document.createElement(el);
+
+    return element;
+  } catch (err) {
+    throw Error(err, "something went wrong check element name");
+  }
+}
+
+function createParaText(para_number) {
+  const paraElementStr =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, voluptatibus, sequi atque nemo velit dolorem expedita minima consequatur ratione iste et quae explicabo culpa aliquid, ea unde quasi tempora? Sed.";
+  let arr = [];
+  for (let i = 1; i <= para_number; i++) {
+    let para = createEl("p");
+    para.textContent = paraElementStr;
+    arr.push(para);
+  }
+
+  return arr;
+}
+const bgImage = createEl("div");
+bgImage.className += "bg-image ";
+bgImage.id = "bg-image";
+
+const container = createEl("div");
+container.className += "container";
+const h1 = createEl("h1");
+h1.textContent = "welcome to our website";
+container.appendChild(h1);
+
+const paraElements = createParaText(10);
+
+paraElements.forEach((el) => container.appendChild(el));
+console.log(container, bgImage);
+document.body.appendChild(bgImage)
+document.body.appendChild(container)
